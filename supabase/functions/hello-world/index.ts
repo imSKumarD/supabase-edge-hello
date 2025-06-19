da17c306b37f5d11457f9be261940bbfda344a88
imSKumarD <shailesh.dubey1@gmail.com>
2025-06-20 02:23:38 +0530

    Update index.ts

--- supabase/functions/hello-world/index.ts
+++ supabase/functions/hello-world/index.ts
@@ -2,7 +2,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts"
 
 serve((req) => {
-  return new Response("Hello from Supabase Edge Function!", {
+  return new Response("Supabase Edge Function Hello!", {
     headers: { "Content-Type": "text/plain" },
   })
 })
